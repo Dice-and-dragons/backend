28ff0a8351725574ef64c6beceacff0949ad3480
Anton Mykolenko <119318292+AntonMykolenko@users.noreply.github.com>
2024-07-23 13:38:42 +0300

    added character type

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import http from "http";
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { Server } from "socket.io";
-import { db } from "@utils/database";
-import { characters } from "@schemas/dbSchema";
 import { CharacterRouter } from "./routes/character";
 import { socketServer } from "@socket/socket";
 
@@ -15,15 +13,8 @@ routes.forEach((route) => {
   app.use(route.path, route.router);
 });
 
-// Set up your Express routes, middleware, etc.
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
-
-// Create HTTP server
 const server = http.createServer(app);
 
-// Integrate Socket.io
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -32,7 +23,6 @@ const io = new Server(server, {
 
 socketServer(io);
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
