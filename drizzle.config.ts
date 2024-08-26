5364675a2eafd57d89b31219c9af2b3496e59cca
Anton Mykolenko <119318292+AntonMykolenko@users.noreply.github.com>
2024-08-26 17:25:37 +0300

    updated database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,11 @@ export default defineConfig({
   schema: "./src/schemas/dbSchema.ts",
   out: "./migrations",
   dbCredentials: {
-    host: "dnd-db-stack-dev-dbinstance-jpzjxchlei3h.crcgoa4wie4d.eu-central-1.rds.amazonaws.com",
-    port: 5432,
-    user: "dnddb",
-    password: "xu5JY4do8A6S15rGr{)vJ){4Y*6l",
-    database: "dev_dnd_db_stack",
+    host: "autorack.proxy.rlwy.net",
+    port: 10119,
+    user: "postgres",
+    password: "UapGytNpgWCrLbqJmQgxrbTskUtwPsDP",
+    database: "railway",
     ssl: { rejectUnauthorized: false },
   },
 });
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -2,11 +2,11 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 
 const postgresClient = postgres({
-  host: "dnd-db-stack-dev-dbinstance-jpzjxchlei3h.crcgoa4wie4d.eu-central-1.rds.amazonaws.com",
-  port: 5432,
-  user: "dnddb",
-  password: "xu5JY4do8A6S15rGr{)vJ){4Y*6l",
-  database: "dev_dnd_db_stack",
+  host: "autorack.proxy.rlwy.net",
+  port: 10119,
+  user: "postgres",
+  password: "UapGytNpgWCrLbqJmQgxrbTskUtwPsDP",
+  database: "railway",
   ssl: { rejectUnauthorized: false },
 });
 
